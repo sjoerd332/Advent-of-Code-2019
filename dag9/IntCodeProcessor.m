e2fe71c3dd0b0f5737147451289c3acdd89a71e5
nijhofsfj <s.f.j.nijhof@student.utwente.nl>
2019-12-22 22:05:28 +0100

    part 1 complete

--- dag9/IntCodeProcessor.m
+++ dag9/IntCodeProcessor.m
@@ -23,67 +23,68 @@ while(1)
     obj.haltCondition = 2;
     break;
   end;
-  if(obj.relativeBase > length(obj.memory) || obj.relativeBase < 1)
+  if(obj.relativeBase > length(obj.memory))
     display('ERR: relative base out of bounds!');
     obj.haltCondition = 6;
     break;
   end;
-  if(currentInstruction == 1) % add
+  switch currentInstruction
+  case 1 % add
     %obj.memory(obj.memory(obj.currentInstructionAddress+3)+1) = ...
-    obj.memory(obj.writeToMemory(p3,3)) = ...
+    obj.memory(obj.getWriteAdress(p3,3)) = ...
     obj.getParameterModeOutput(p1,obj.memory(obj.currentInstructionAddress+1)) + ...
     obj.getParameterModeOutput(p2,obj.memory(obj.currentInstructionAddress+2));
     obj.currentInstructionAddress += 4;
-  elseif(currentInstruction == 2) % multiply
+  case 2 % multiply
     %obj.memory(obj.memory(obj.currentInstructionAddress+3)+1) = ...
-    obj.memory(obj.writeToMemory(p3,3)) = ...
+    obj.memory(obj.getWriteAdress(p3,3)) = ...
     obj.getParameterModeOutput(p1,obj.memory(obj.currentInstructionAddress+1)) * ...
     obj.getParameterModeOutput(p2,obj.memory(obj.currentInstructionAddress+2));
     obj.currentInstructionAddress += 4;
-  elseif(currentInstruction == 3) % load input
+  case 3 % load input
     inputCnt +=1;
     if(inputCnt > length(obj.input))
       obj.haltCondition = 3;
       break;
     end
     %obj.memory(obj.memory(obj.currentInstructionAddress+1)+1) = obj.input(inputCnt);
-    obj.memory(obj.writeToMemory(p1,1)) = obj.input(inputCnt);
+    obj.memory(obj.getWriteAdress(p1,1)) = obj.input(inputCnt);
     obj.currentInstructionAddress += 2;
-  elseif(currentInstruction == 4) % load output
+  case 4 % load output
     outputCnt += 1;   
     obj.output(outputCnt) = obj.getParameterModeOutput(p1,obj.memory(obj.currentInstructionAddress+1));
     obj.currentInstructionAddress += 2;
-  elseif(currentInstruction == 5) % jump if true
+  case 5 % jump if true
     if(obj.getParameterModeOutput(p1,obj.memory(obj.currentInstructionAddress+1)) != 0)
       obj.currentInstructionAddress = obj.getParameterModeOutput(p2,obj.memory(obj.currentInstructionAddress+2)) +1;
     else
       obj.currentInstructionAddress += 3;
     end
-  elseif(currentInstruction == 6) % jump if false
+  case 6 % jump if false
     if(obj.getParameterModeOutput(p1,obj.memory(obj.currentInstructionAddress+1)) == 0)
       obj.currentInstructionAddress = obj.getParameterModeOutput(p2,obj.memory(obj.currentInstructionAddress+2)) +1;
     else
       obj.currentInstructionAddress += 3;
     end
-  elseif(currentInstruction == 7) % less than
+  case 7 % less than
     %obj.memory(obj.memory(obj.currentInstructionAddress+3)+1) = ...
-    obj.memory(obj.writeToMemory(p3,1)) = ...
+    obj.memory(obj.getWriteAdress(p3,3)) = ...
     obj.getParameterModeOutput(p1,obj.memory(obj.currentInstructionAddress+1)) < ...
     obj.getParameterModeOutput(p2,obj.memory(obj.currentInstructionAddress+2));
     obj.currentInstructionAddress += 4;
-  elseif(currentInstruction == 8) % equals
+  case 8 % equals
     %obj.memory(obj.memory(obj.currentInstructionAddress+3)+1) = ...
-    obj.memory(obj.writeToMemory(p3,1)) = ...
+    obj.memory(obj.getWriteAdress(p3,3)) = ...
     obj.getParameterModeOutput(p1,obj.memory(obj.currentInstructionAddress+1)) == ...
     obj.getParameterModeOutput(p2,obj.memory(obj.currentInstructionAddress+2));
     obj.currentInstructionAddress += 4;
-  elseif(currentInstruction == 9)
+  case 9 % modify relative base
     obj.relativeBase = obj.relativeBase + obj.getParameterModeOutput(p1,obj.memory(obj.currentInstructionAddress+1));
-    obj.currentInstructionAddress +=2;
-  elseif(currentInstruction == 99)
+    obj.currentInstructionAddress += 2;
+  case 99 % controlled halt
     obj.haltCondition = 1;
     break;
-  else
+  otherwise
     display('ERR: instruction not implemented!');
     obj.haltCondition = 4;
     break;
@@ -92,7 +93,7 @@ end
 end
 
 function [p3 p2 p1 instruction] = getInstruction(obj,inp)
-  p3 = 0;%floor(inp/10000);
+  p3 = floor(inp/10000);
   p2 = floor((inp-10000*p3)/1000);
   p1 = floor((inp-1000*p2-10000*p3)/100);
   instruction = rem(inp,100);
@@ -106,20 +107,20 @@ function outp = getParameterModeOutput(obj,p,inp)
     outp = inp;
   end
   if(p == 2) # value at relative address, relative mode
-    outp = obj.memory(obj.relativeBase +1 + inp);
-  end
+    outp = obj.memory(obj.relativeBase + inp +1);
+  end 
 end
 
-function outp = writeToMemory(obj,p,inp)
+function outp = getWriteAdress(obj,p,offsetFromInstruction)
   if(p == 0)
-    outp = obj.memory(obj.currentInstructionAddress + inp) +1;
+    outp = obj.memory(obj.currentInstructionAddress + offsetFromInstruction) +1;
   end
   if(p == 1)
     obj.haltCondition = 5;
     display('ERR: not implemented!');
   end
   if(p == 2)
-    outp = obj.relativeBase + inp;
+    outp = obj.relativeBase + obj.memory(obj.currentInstructionAddress + offsetFromInstruction )+1;
   end
 end  
 
