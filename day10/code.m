eaf0b8b1adf7734cbc238ef1c8052ed667fd0440
nijhofsfj <s.f.j.nijhof@student.utwente.nl>
2019-12-24 16:27:46 +0100

    update directory structure

--- day10/code.m
+++ day10/code.m
@@ -6,8 +6,8 @@ close all
 ##filename = 'ex1.txt';
 ##filename = 'ex2.txt';
 ##filename = 'ex3.txt';
-##filename =  'ex4.txt';
-filename = 'input.txt';
+filename =  'ex4.txt';
+##filename = 'input.txt';
 
 input = textread(filename,'%s');
 xLength = length(input(1){:});
@@ -23,12 +23,14 @@ for y = 1:yLength
     end
 end
 max1 = 0;
-for i = 1:astroids
+for i = 206 %1:astroids % speedup part 2
     for j = 1:astroids
         if(i != j)
             ang(i,j) = atan2((astroid(j,2)-astroid(i,2)),(astroid(j,1)-astroid(i,1)));
+            rad(i,j) = sqrt((astroid(j,2)-astroid(i,2))^2 + (astroid(j,1)-astroid(i,1))^2 );
         else
             ang(i,j) = Inf;
+            rad(i,j) = 0;
         end 
     end
     uniques{i} = unique(ang(i,:));
@@ -36,9 +38,36 @@ for i = 1:astroids
         max1 = length(uniques{i})-1; % compensate for self length
         coordinate1 = astroid(i,1)-1; % compensate 1-based grid
         coordinate2 = astroid(i,2)-1;
+        astroidNr = i;
     end
 end
 
 max1
 coordinate1
 coordinate2
+
+% part 2
+% remove current astroid from list
+ang = ang(astroidNr,([1:astroidNr-1,astroidNr+1:end]));
+rad = rad(astroidNr,([1:astroidNr-1,astroidNr+1:end]));
+astroid = astroid([1:astroidNr-1,astroidNr+1:end],:);
+%map all angles between pi/2 and pi/2 + 2 pi
+ang = ang ;%+ pi * 1.5;
+%start angle
+angle = pi/2;
+unVaporizedAstroids = astroid;
+vaporizedAstroids = [];
+while(length(unVaporizedAstroids) > 0)
+    differenceAngle = ang - angle;
+%    differenceAngle(differenceAngle == 0) = Inf;% exclude equal angles
+%    signAngle = 2*(differenceAngle>0)-1;
+    minDifferenceAngle = min(signAngle.*differenceAngle);
+    sameAngles = find(differenceAngle == minDifferenceAngle);
+    [minRad,zapIdx] = min(rad(sameAngles));
+    vaporizedAstroids = [vaporizedAstroids;unVaporizedAstroids(zapIdx,:)];
+    angle = ang(zapIdx);
+    unVaporizedAstroids(zapIdx,:) = [];
+    ang(zapIdx) = [];
+    rad(zapIdx) = [];
+    signAngle = [];
+end
