7e6d90a79bbd64656d254be0579bb211d20176d8
nijhofsfj <s.f.j.nijhof@student.utwente.nl>
2019-12-26 12:01:34 +0100

    day 10 part 2 done

--- day10/code.m
+++ day10/code.m
@@ -6,8 +6,8 @@ close all
 ##filename = 'ex1.txt';
 ##filename = 'ex2.txt';
 ##filename = 'ex3.txt';
-filename =  'ex4.txt';
-##filename = 'input.txt';
+%filename =  'ex4.txt';
+filename = 'input.txt';
 
 input = textread(filename,'%s');
 xLength = length(input(1){:});
@@ -23,7 +23,7 @@ for y = 1:yLength
     end
 end
 max1 = 0;
-for i = 206 %1:astroids % speedup part 2
+for i = 1:astroids % 206% speedup part 2
     for j = 1:astroids
         if(i != j)
             ang(i,j) = atan2((astroid(j,2)-astroid(i,2)),(astroid(j,1)-astroid(i,1)));
@@ -54,20 +54,35 @@ astroid = astroid([1:astroidNr-1,astroidNr+1:end],:);
 %map all angles between pi/2 and pi/2 + 2 pi
 ang = ang ;%+ pi * 1.5;
 %start angle
-angle = pi/2;
+angle = -pi/2;
 unVaporizedAstroids = astroid;
 vaporizedAstroids = [];
+skipEqualAngles = false;
+zapCount = 0;
+zapAngle = [];
 while(length(unVaporizedAstroids) > 0)
-    differenceAngle = ang - angle;
-%    differenceAngle(differenceAngle == 0) = Inf;% exclude equal angles
-%    signAngle = 2*(differenceAngle>0)-1;
-    minDifferenceAngle = min(signAngle.*differenceAngle);
-    sameAngles = find(differenceAngle == minDifferenceAngle);
+    if(skipEqualAngles)
+        differenceAngle = ang + (ang<=angle)*2*pi - angle;
+    else
+        differenceAngle = ang + (ang<angle)*2*pi - angle;
+    end
+
+    maxDifferenceAngle = min(differenceAngle);
+    sameAngles = find(differenceAngle == maxDifferenceAngle);
+    if(length(sameAngles) > 1)
+        skipEqualAngles = true;
+    else
+        skipEqualAngles = false;
+    end
     [minRad,zapIdx] = min(rad(sameAngles));
-    vaporizedAstroids = [vaporizedAstroids;unVaporizedAstroids(zapIdx,:)];
-    angle = ang(zapIdx);
-    unVaporizedAstroids(zapIdx,:) = [];
-    ang(zapIdx) = [];
-    rad(zapIdx) = [];
-    signAngle = [];
+    vaporizedAstroids = [vaporizedAstroids;unVaporizedAstroids(sameAngles(zapIdx),:)];
+    zapCount += 1;
+    zapAngle(zapCount) = angle;
+    angle = ang(sameAngles(zapIdx));
+    unVaporizedAstroids(sameAngles(zapIdx),:) = [];
+    ang(sameAngles(zapIdx)) = [];
+    rad(sameAngles(zapIdx)) = [];
+    differenceAngle = [];
 end
+
+ansPart2 = (vaporizedAstroids(200,1)-1)*100+(vaporizedAstroids(200,2)-1)
